2731daf3c2c2ddae714380a97326e7bdea875e4c
kwakaflocka <sabrinakwak@gmail.com>
2024-11-06 19:46:34 -0700

    e

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -58,7 +58,8 @@ const about = {
         display: true
     },
     calendar: {
-        display: false
+        display: true,
+        link: 'https://cal.com'
     },
     intro: {
         display: true,
